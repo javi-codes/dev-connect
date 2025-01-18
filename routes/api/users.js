75feb0a02c65986052d4d2b9ba5afde7613a12d0
Javier Ramirez Roa <j.ramirez.roa64@gmail.com>
2025-01-17 23:11:56 -0800

    Added validators to users route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,28 @@
 const router = require("express").Router();
+const { check, validationResult } = require("express-validator");
 
-// @route  GET api/users
+// @route  POST  api/users
 // @desc   Test route
 // @access Public
-router.get("/", (req, res) => {
-  res.send("User route");
-});
+router.post(
+  "/",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ erros: errors.array() });
+    }
+    console.log(req.body);
+
+    res.send("User route");
+  }
+);
 
 module.exports = router;
