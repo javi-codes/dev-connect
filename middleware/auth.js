f0e80a5a746cef88d0c5c475e45e9313b5964a16
Javier Ramirez Roa <j.ramirez.roa64@gmail.com>
2025-01-23 23:39:42 -0800

    Auth middleware and auth route

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,7 +7,7 @@ module.exports = function (req, res, next) {
 
   // check if no token exists
   if (!token) {
-    return res.stat(401).json({ msg: "No token, authorization denied" });
+    return res.status(401).json({ msg: "No token, authorization denied" });
   }
 
   // verify token
