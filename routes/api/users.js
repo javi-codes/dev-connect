127ed60fdf2efb0ecf1e5093479081e3a058139d
Javier Ramirez Roa <j.ramirez.roa64@gmail.com>
2025-01-25 23:47:28 -0800

    Small change to description of the route

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,7 +8,7 @@ const config = require("config");
 const User = require("../../models/User");
 
 // @route  POST  api/users
-// @desc   Test route
+// @desc   Create user
 // @access Public
 router.post(
   "/",
