cfec2994421837bfdeaa5b5d457bf1bf170c31ee
Javier Ramirez Roa <j.ramirez.roa64@gmail.com>
2025-01-25 22:51:08 -0800

    Created route to create profile  with relevant data.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -58,9 +58,10 @@ router.post(
       instagram,
       linkedin,
     } = req.body;
+
     // Build profile object
     const profileFields = {};
-    profileFields.user - req.user.id;
+    profileFields.user = req.user.id;
 
     if (company) profileFields.company = company;
     if (website) profileFields.website = website;
@@ -69,10 +70,39 @@ router.post(
     if (status) profileFields.status = status;
     if (githubusername) profileFields.githubusername = githubusername;
     if (skills) {
-      profileFields.skills = skills.split(",").map((skill) => skill.trim);
+      profileFields.skills = skills.split(",").map((skill) => skill.trim());
     }
-    console.log(skills);
-    res.send("hello");
+
+    // Social Object
+    profileFields.social = {};
+    if (youtube) profileFields.social.youtube = youtube;
+    if (facebook) profileFields.social.facebook = facebook;
+    if (twitter) profileFields.social.twitter = twitter;
+    if (linkedin) profileFields.social.linkedin = linkedin;
+    if (instagram) profileFields.social.instagram = instagram;
+
+    try {
+      let profile = await Profile.findOne({ user: req.user.id });
+      if (profile) {
+        // update
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          {
+            $set: profileFields,
+          },
+          { new: true }
+        );
+        return res.json(profile);
+      }
+
+      // create
+      profile = new Profile(profileFields);
+      await profile.save();
+      res.json(profile);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server Error");
+    }
   }
 );
 
