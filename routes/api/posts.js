a883bb4b0f8ed258d98a74a97cfc9a96dc767929
Javier Ramirez Roa <j.ramirez.roa64@gmail.com>
2025-01-29 16:54:36 -0800

    Changed some code to better handle a CastError that was coming up in the terminal

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { check, validationResult } = require("express-validator");
+const { default: mongoose } = require("mongoose");
 
 const auth = require("@middleware/auth");
 
@@ -76,10 +77,12 @@ router.get("/:id", auth, async (req, res) => {
 // @access Private
 router.delete("/:id", auth, async (req, res) => {
   try {
-    const post = await Post.findById(req.params.id);
-    if (!post) {
+    // check if req.params.id is valid object
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
       return res.status(404).json({ msg: "Post not found" });
     }
+
+    const post = await Post.findById(req.params.id);
 
     // Check that user owns the post
     if (post.user.toString() !== req.user.id) {
@@ -90,9 +93,7 @@ router.delete("/:id", auth, async (req, res) => {
     res.json({ msg: "Post removed" });
   } catch (error) {
     console.error(error.message);
-    if (error.name === "CastError") {
-      return res.status(404).json({ msg: "Post not found" });
-    }
+
     res.status(500).send("Server Error");
   }
 });
