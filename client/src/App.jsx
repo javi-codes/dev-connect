9bea0823e6bddad51895b412c432230df31e8b22
Javier Ramirez Roa <j.ramirez.roa64@gmail.com>
2025-02-05 16:40:09 -0800

    Working on client side of this app.

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+// import "./App.css";
+
+const App = () => (
+  <>
+    <h1>Dev Connector</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere, aliquid.
+      Modi nobis eveniet, fugit itaque dignissimos incidunt illum officiis
+      praesentium dicta quia laborum tenetur at quod, fuga tempora, deleniti
+      sit?
+    </p>
+  </>
+);
+
+export default App;
