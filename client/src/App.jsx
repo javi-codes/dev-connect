0bc8c8c70e6ad4e3e3da39d7f779c7522819ed0f
Javier Ramirez Roa <j.ramirez.roa64@gmail.com>
2025-02-05 18:00:40 -0800

    Set up the layout components for landing page and navbar.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,13 @@
 import React from "react";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
 
-// import "./App.css";
+import "./App.css";
 
 const App = () => (
   <>
-    <h1>Dev Connector</h1>
-    <p>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere, aliquid.
-      Modi nobis eveniet, fugit itaque dignissimos incidunt illum officiis
-      praesentium dicta quia laborum tenetur at quod, fuga tempora, deleniti
-      sit?
-    </p>
+    <Navbar />
+    <Landing />
   </>
 );
 
